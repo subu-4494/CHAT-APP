130c44a8b420e1744cef32932d006338db5e9c42
Sourabh <sourabroy2018@gmail.com>
2024-12-27 03:34:01 +0530

    settingsPage done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,13 @@ import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect } from "react";
 import {Loader} from "lucide-react";
 import {Toaster} from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 const App = () => {
 
   const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
+                  
+    const { theme } =    useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -28,7 +31,7 @@ const App = () => {
   );
 
   return  (
-  <div >
+    <div data-theme= {theme}>
 
    <Navbar />
 
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -91,7 +91,7 @@ const ProfilePage = () => {
               <div className="flex items-center justify-between py-2 border-b border-zinc-700">
                 <span>Member Since</span>
                 <span>{authUser.createdAt?.split("T")[0]|| "Date not available"}</span>
-              </div>
+                </div>
               <div className="flex items-center justify-between py-2">
                 <span>Account Status</span>
                 <span className="text-green-500">Active</span>
