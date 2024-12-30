9b1828a46a968862e628b5d8ac0395b31e903d53
Sourabh <sourabroy2018@gmail.com>
2024-12-30 14:49:45 +0530

    sidebar created + some issues resolved

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -113,10 +113,10 @@ export const updateProfile = async(req,res) => {
 
 export const checkAuth = async (req, res) => {
     try {
-        const response = await axios.get('/api/auth/check', { withCredentials: true });
+       // const response = await axios.get('/api/auth/check', { withCredentials: true });
         res.status(200).json(req.user);
     } catch (error) {
         console.log("Error in checkAuth controller", error.message);
         res.status(500).json({message : "Internal Server Error"});
     }
-}
+};
