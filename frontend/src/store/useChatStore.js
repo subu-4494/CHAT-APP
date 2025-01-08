ceb223fff632819edffc51d6d54e67f870968f75
Sourabh <sourabroy2018@gmail.com>
2025-01-08 20:09:15 +0530

    messages part

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -34,6 +34,16 @@ export const useChatStore = create((set, get) => ({
     }
   },
 
+  sendMessage: async(messageData) => {
+       const{selectedUser,messages}  =get();
+       try {
+        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+        set({ messages: [...messages, res.data] });
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+  },
+
   // optimise this later
 
   setSelectedUser: (selectedUser)  => set({selectedUser}),
