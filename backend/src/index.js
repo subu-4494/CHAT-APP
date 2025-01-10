2d1911a4254caacbad3328782fab0769c7e6bfe4
Sourabh <sourabroy2018@gmail.com>
2025-01-11 02:36:48 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import {connectDB} from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +15,8 @@ dotenv.config();
 
 
 const PORT = process.env.PORT||5174;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,6 +36,15 @@ app.use((req, res, next) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+   
+     app.get("*", (req, res) => {
+       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+     });
+   }
+   
 
 server.listen(PORT, () =>{
      console.log("server is running on port PORT:" + PORT);
